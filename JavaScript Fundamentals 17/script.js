b93e15b9e43b83cfe94d7cce270f29217f20a189
Mario-Gozie <oguedoihucg@gmail.com>
2024-11-08 17:25:23 +0200

    Refactoring the project archetecture and creating the initial app object and initial processes when the site loads

--- JavaScript Fundamentals 17/script.js
+++ JavaScript Fundamentals 17/script.js
@@ -12,75 +12,109 @@ const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
 // Declaring 2 variables I want to easily have access to even if I declare them inside a code block.
-let map, mapEvent;
-// How to use the geolocation API
-if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      //   console.log(position);
+// let map, mapEvent;
 
-      // const  latitud  = position.coords.latitude;
+class App {
+  #map;
 
-      // instead of doing the above to get latitude, we did destructuring below, we also applied the same destructuring to get the longitude.
+  #mapEvent;
+  constructor() {
+    this._getPosition(); //after creating the new app object, this is the first code to run to provide position
 
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
+    form.addEventListener("submit", this._newWorkout.bind(this)); // This keyword will always be the element it is attached to. so to set the this keyword to the app object created, you need to use bind.
 
-      // With this longitude and latitude, we can centre the load the map by using the longitude and latitude on google map link.
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    inputType.addEventListener("change", this._toggleElevationField);
+  }
 
-      const coords = [latitude, longitude];
-      map = L.map("map").setView(coords, 13);
-      // console.log(map);
+  // GETTING POSITION METHOD
+  _getPosition() {
+    // How to use the geolocation API
+    // This conde is used to get current location.
+    // Then this same code also calls the load map fuction to load the map of the location gotten.
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert(`Could not get your position`);
+        }
+      );
+  }
 
-      L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+  // LOADING MAP METHOD
 
-      // Handling Clicks on maps (Just to display form)
-      map.on("click", function (mapE) {
-        mapEvent = mapE;
-        form.classList.remove("hidden");
-        inputDistance.focus();
-      });
-    },
-    function () {
-      alert(`Could not get your position`);
-    }
-  );
+  // The code below is used to load a map with the current positon.
+  _loadMap(position) {
+    //   console.log(position);
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
+    // const  latitud  = position.coords.latitude;
 
-  // Clear Input fields
-  inputDistance.value =
-    inputDuration.value =
-    inputCadence.value =
-    inputElevation.value =
-      "";
+    // instead of doing the above to get latitude, we did destructuring below, we also applied the same destructuring to get the longitude.
 
-  // Display the marker
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
 
-  console.log(mapEvent);
-  const { lat, lng } = mapEvent.latlng;
-  console.log(lat, lng);
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        naxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: "running-popup",
-      })
-    )
-    .setPopupContent("Workout")
-    .openPopup();
-});
+    // With this longitude and latitude, we can centre the load the map by using the longitude and latitude on google map link.
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-inputType.addEventListener("change", function () {
-  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
-  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
-});
+    const coords = [latitude, longitude];
+    this.#map = L.map("map").setView(coords, 13);
+    // console.log(map);
+
+    L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    // Handling Clicks on maps (Just to display form)
+    this.#map.on("click", this._showForm.bind(this));
+  }
+
+  // SHOW FORM METHOD
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove("hidden");
+    inputDistance.focus();
+  }
+
+  // TOGGLE ELEVATION FIELD METHOD
+
+  _toggleElevationField() {
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  }
+
+  // NEW WORKOUT METHOD
+  _newWorkout(e) {
+    e.preventDefault();
+
+    // Clear Input fields
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        "";
+
+    // Display the marker
+
+    const { lat, lng } = this.#mapEvent.latlng;
+    console.log(lat, lng);
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          naxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: "running-popup",
+        })
+      )
+      .setPopupContent("Workout")
+      .openPopup();
+  }
+}
+
+// CREATING A NEW APP OBJECT
+
+const app = new App();
