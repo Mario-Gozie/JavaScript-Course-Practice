391bed53b0e2f4a824f2c579cd5f71cbe1ffd2bd
Mario-Gozie <oguedoihucg@gmail.com>
2024-11-08 03:39:37 +0200

    done with inputing the data fetching the map and setting it up

--- JavaScript Fundamentals 17/script.js
+++ JavaScript Fundamentals 17/script.js
@@ -16,10 +16,51 @@ if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
       //   console.log(position);
+
+      // const  latitud  = position.coords.latitude;
+
+      // instead of doing the above to get latitude, we did destructuring below, we also applied the same destructuring to get the longitude.
+
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      //   console.log(latitude, longitude);
+
+      // With this longitude and latitude, we can centre the load the map by using the longitude and latitude on google map link.
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+      const map = L.map("map").setView(coords, 13);
+      // console.log(map);
+
+      L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      // L.marker(coords)
+      //   .addTo(map)
+      //   .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+      //   .openPopup();
+
+      map.on("click", function (mapEvent) {
+        console.log(mapEvent);
+
+        const { lat, lng } = mapEvent.latlng;
+        console.log(lat, lng);
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              naxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: "running-popup",
+            })
+          )
+          .setPopupContent("Workout")
+          .openPopup();
+      });
     },
     function () {
       alert(`Could not get your position`);
