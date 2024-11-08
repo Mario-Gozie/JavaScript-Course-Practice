40a60097db5853d5eaee11562ee2acb9a8830ad2
Mario-Gozie <oguedoihucg@gmail.com>
2024-11-08 14:26:14 +0200

    done with displaying the form

--- JavaScript Fundamentals 17/script.js
+++ JavaScript Fundamentals 17/script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+// Declaring 2 variables I want to easily have access to even if I declare them inside a code block.
+let map, mapEvent;
 // How to use the geolocation API
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
@@ -28,7 +30,7 @@ if (navigator.geolocation)
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
       const coords = [latitude, longitude];
-      const map = L.map("map").setView(coords, 13);
+      map = L.map("map").setView(coords, 13);
       // console.log(map);
 
       L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
@@ -36,33 +38,49 @@ if (navigator.geolocation)
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      // L.marker(coords)
-      //   .addTo(map)
-      //   .bindPopup("A pretty CSS popup.<br> Easily customizable.")
-      //   .openPopup();
-
-      map.on("click", function (mapEvent) {
+      // Handling Clicks on maps (Just to display form)
+      map.on("click", function (mapE) {
+        mapEvent = mapE;
         form.classList.remove("hidden");
         inputDistance.focus();
-        // console.log(mapEvent);
-        // const { lat, lng } = mapEvent.latlng;
-        // console.log(lat, lng);
-        // L.marker([lat, lng])
-        //   .addTo(map)
-        //   .bindPopup(
-        //     L.popup({
-        //       naxWidth: 250,
-        //       minWidth: 100,
-        //       autoClose: false,
-        //       closeOnClick: false,
-        //       className: "running-popup",
-        //     })
-        //   )
-        //   .setPopupContent("Workout")
-        //   .openPopup();
       });
     },
     function () {
       alert(`Could not get your position`);
     }
   );
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // Clear Input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      "";
+
+  // Display the marker
+
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+  console.log(lat, lng);
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        naxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent("Workout")
+    .openPopup();
+});
+
+inputType.addEventListener("change", function () {
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+});
