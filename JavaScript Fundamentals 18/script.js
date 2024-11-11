fe90876138d0523da684e6904b23b88a5b494d8f
Mario-Gozie <oguedoihucg@gmail.com>
2024-11-11 03:50:35 +0200

    done with the first theory part of Asynchronous JavaScript

--- JavaScript Fundamentals 18/script.js
+++ JavaScript Fundamentals 18/script.js
@@ -1,6 +1,25 @@
-'use strict';
+"use strict";
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
+
+// Understanding Synchronous and Asynchronous JavaScript
+
+// Synchronous Javascript is a group of JavaScript codes that are executed line by line. and when this is the case, all code has to wait for code before it to finish running before it can run. A very good example is the alert popup. it will stop other codes from running unless we press ok, then it can allow other codes below it to run.
+
+// In Asynchronous codes, the codes that take shorter time to run would not have to wait for codes that take longer time to run. A very good exaqmple is the setTimeout function which run after a particular time. it doesn't stop synchronous JavaScript codes from running. Asynchronous Javascript does not block other codes from running even if it takes a longer time.
+
+// A callback function is in most cases needed to implement Asynchronous behaviour but that does not mean call back functions automatically make a code asynchronous.
+
+// Please note that adding an event listener to an element does not make it asynchronous.
+
+// Asynchronous Javascript is used in AJAX which means Asynchronous Javascript And XML: Allows us to communicate with remote web servers in an asynchronous way. With AJAX calls, we can request data from web services dynamically.
+
+// What is a web API?
+// API stands for Application Programming Interface: in general, it simply means a piece of software that can be used by another software, in other to allow aplication talk to each other.
+
+// an online API is an application running on a webserver, that recieves requests for data, and sends data back as response.
+
+// XML is a data format used to be widely used to transfer data on the web. these days, XML are not used. Instead, most use the JASON format. Jason is more like a javaScript object converted to a string.
