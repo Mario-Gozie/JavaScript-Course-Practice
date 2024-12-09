65255ee4c33c39497af476446a9d036a5ff24af5
Mario-Gozie <oguedoihucg@gmail.com>
2024-12-10 00:42:13 +0200

    was able to do my first import and export

--- JavaScript Fundamentals 19/script.js
+++ JavaScript Fundamentals 19/script.js
@@ -1,3 +1,9 @@
 // IMPORTING MODULE
-import './shopingCart.js';
+import { addToCart } from './shopingCart.js';
+
 console.log('Importing module');
+
+// The code below will display the shippingCost variable in the console because in the module where it is created, the word export was not used to give permision to export it.
+// console.log(shippingCost);
+
+addToCart('bread', 5); // when you run this imported function, it will run perfetly and print what you want on the console because it was exported.
--- JavaScript Fundamentals 19/shopingCart.js
+++ JavaScript Fundamentals 19/shopingCart.js
@@ -1,2 +1,10 @@
 // EXPORTING MODULE
 console.log('Exporting module');
+
+const shippingCost = 10;
+const cart = [];
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
